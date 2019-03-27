3d9336d7e87159930737ee2964a1b545d6dff153
Ang Li <megathere@gmail.com>
2019-03-27 16:48:09 -0700

    first commit

---
+++ src/main/java/hello/HelloController.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class HelloController {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+}
