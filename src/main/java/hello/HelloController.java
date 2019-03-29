6065679121dc57d92c5af5619bf1c3f3a0a83fb9
Ang Li <megathere@gmail.com>
2019-03-29 11:55:15 -0700

    enable admin client

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,14 +1,30 @@
 package hello;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 class HelloController {
 
+    @Autowired
+    CustomerRepository repository;
+
     @RequestMapping("/")
-    String home() {
-        return "Hello World!";
+    List<Customer> home() {
+        List<Customer> list = new ArrayList<>();
+
+        repository.deleteAll();
+
+        repository.save(new Customer("Jack", "Bauer"));
+        repository.save(new Customer("Chloe", "O'Brian"));
+        for (Customer customer : repository.findAll()) {
+            list.add(customer);
+        }
+
+        return list;
     }
 
 }
